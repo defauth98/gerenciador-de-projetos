d9c85976460023bd632e850860fd6a4ae62782f6
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2024-05-09 08:18:41 -0300

    fix: change page name

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -1,7 +1,7 @@
 import LogoScreen from "@/components/LogoScreen";
 import LoginForm from "./components/LoginForm";
 
-export default function Home() {
+export default function LoginPage() {
   return (
     <div className="flex flex-row w-screen h-screen">
       <LogoScreen />
