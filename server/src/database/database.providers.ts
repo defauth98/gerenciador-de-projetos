2e90759fe15b67405e5fda53ba543229731946e1
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2024-12-21 13:10:35 -0300

    chore: get db connection info from env

--- server/src/database/database.providers.ts
+++ server/src/database/database.providers.ts
@@ -1,4 +1,7 @@
 import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+
+config();
 
 export const databaseProviders = [
   {
@@ -6,11 +9,11 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'docker',
-        database: 'project-manager',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
       });
